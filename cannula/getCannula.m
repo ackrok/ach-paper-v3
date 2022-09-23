16bfa58a8f6200f4f3ff28b0a95fb7bdba1be9b3
ackrok <ackrok@users.noreply.github.com>
2022-09-23 11:40:19 -0400

    update getcannula

--- cannula/getCannula.m
+++ cannula/getCannula.m
@@ -12,10 +12,15 @@ beh = extractBeh(fPath, fName); % extract data
 
 %% Organize data into cannula structure
 infWindow = [20 40]; % window to analyze after infusion, in minutes
-infOptions = {'saline','iGluR antag','nAChR antag','mAChR antag','D1/2R antag','D2R agonist'};
+infOptions = {'saline','iGluR-antag','nAChR-antag','mAChR-antag','D1/2R-antag','D2R-ago','muscimol','other'};
 for x = 1:length(beh)
     choice = menu(sprintf('Select infusion: %s',fName{x}),infOptions); % select data type
     beh(x).inf = infOptions{choice}; % store infusion label into structure
+    if choice == 8
+        infThis = inputdlg({'Enter infusion:'},'Input',1,{''});
+        beh(x).inf = infThis{1};
+    end
+    
 end
 uni = unique({beh.inf}); % unique infusions
 uni = fliplr(uni); % flip so that saline is first
@@ -31,6 +36,6 @@ end
 choice = menu('Save cannula structure for future use?','Yes','No');
 switch choice
     case 1
-        canName = inputdlg('Enter name for file to save cannula structure:','Input',1,{'achda_beh_cannulaDLS_saline+XXinfusion.mat'});
+        canName = inputdlg('Enter name for file to save cannula structure:','Input',1,{['achda_beh_cannulaDLS_',uni{1},'+',uni{2},'.mat']});
         save(fullfile(fPath,canName{1}),'cannula');
 end
